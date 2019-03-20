b9108b221c37d19fcf0481714ccdb9b9f3b31081
Prakash, Satya <satyaa.prakash@outlook.com>
2019-03-19 22:04:19 -0400

    Added UnitTest project and sample unit test code.

---
+++ APITemplate.UnitTest/Service/ApplicationServiceTest.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using APITemplate.Domain.Models;
+using APITemplate.Repository.Interface;
+using APITemplate.Service.Implementation;
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APITemplate.UnitTest.Service
+{
+    [TestClass]
+    public class ApplicationServiceTest
+    {
+        IEnumerable<Application> applications = new List<Application>()
+        {
+            new Application{ AppName ="" },
+            new Application{ AppName ="" },
+        };
+
+        [TestMethod]
+        public void ListAsync()
+        {
+            var applicationRepository = new Mock<IApplicationRepository>();
+            applicationRepository.Setup(x => x.ListAsync()).Returns(Task.FromResult(applications));
+            //var unitOfWork = new Mock<UnitOfWork>();
+            //unitOfWork.Setup(x => x.CompleteAsync()).Returns(Task.FromResult(applications));
+            var applicationService = new ApplicationService(applicationRepository.Object, null);
+            var result = applicationService.ListAsync();
+            Assert.IsNotNull(result);
+        }
+    }
+}
